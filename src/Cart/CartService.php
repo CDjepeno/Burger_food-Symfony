7877bdb06fd6150edf14179ef07715e6b4c03f76
CDjepeno <djepeno.christopher@gmail.com>
2020-12-07 09:52:03 +0100

    Use session for the cart

---
+++ src/Cart/CartService.php
@@ -0,0 +1,135 @@
+<?php
+namespace App\Cart;
+
+use App\Cart\CartItem;
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class CartService {
+
+    protected $session;
+    protected $productRepo;
+
+    public function __construct(SessionInterface $session, ProductRepository $productRepo)
+    {
+        $this->session = $session;
+        $this->productRepo = $productRepo;
+    }
+
+    protected function getCart(): array
+    {
+        return $this->session->get("cart",[]);
+    }
+
+    protected function saveCart(array $cart)
+    {
+        return $this->session->set("cart",$cart);
+    }
+
+    /**
+     * Permet d'ajouter un produit au panier
+     *
+     * @param integer $id
+     * 
+     * @return void
+     * 
+     */
+    public function add(int $id): void
+    {
+        $cart = $this->getCart();
+        
+        if(!array_key_exists($id, $cart)){
+            $cart[$id] = 0;
+        }
+        
+        $cart[$id]++;
+     
+        $this->saveCart($cart);
+    }
+
+    /**
+     * Permet de calculer le total du panier
+     *
+     * @return integer
+     */
+    public function getTotal(): int
+    {
+        $total = 0;
+
+        foreach($this->getCart() as $id => $quantity ) {
+            $product = $this->productRepo->find($id);
+
+            if(!$product){
+                continue;
+            }
+            $total += ($product->getPrice() * $quantity);
+        }
+
+        return $total;
+    }
+
+    /**
+     * Permet d'avoir le detail du panier
+     *
+     * @return array
+     */
+    public function getDetailedCart(): array
+    {
+        $cartDetailed = [];
+
+        foreach($this->getCart() as $id => $quantity ) {
+            $product = $this->productRepo->find($id);
+            
+            if(!$product){
+                continue;
+            }
+            
+            $cartDetailed[]= new CartItem($product, $quantity);
+
+        }
+
+        return $cartDetailed;
+    }
+
+    /**
+     * Permet de supprimer un produit au panier
+     *
+     * @param [int] $id
+     * 
+     * @return void
+     */
+    public function remove(int $id): void
+    {
+        $cart = $this->getCart();
+
+        unset($cart[$id]);
+
+        $this->saveCart($cart);
+    }
+
+    /**
+     * Permet de décrémenter un produit du panier
+     *
+     * @param [int] $id
+     * 
+     * @return void
+     */
+    public function decrement(int $id): void
+    {
+        $cart = $this->getCart();
+
+        if(!array_key_exists($id,$cart)){
+            return;
+        }
+
+        if($cart[$id] === 1) {
+            $this->remove($id);
+            return;
+        }
+
+        $cart[$id]--;
+
+        $this->saveCart($cart);
+
+    }
+}
---
+++ src/Controller/CartController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Controller;
+
+use App\Cart\CartService;
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+
+class CartController extends AbstractController
+{
+    protected $productRepo;
+    protected $cartService;
+
+    public function __construct(ProductRepository $productRepo, CartService $cartService)
+    {
+        $this->productRepo = $productRepo;
+        $this->cartService = $cartService;
+    }
+
+    /**
+     * Permet de d'ajouter un produit au panier
+     * 
+     * @Route("/customer/cart/add/{id<\d+>}", name="cart_add")
+     * 
+     * @param int $id
+     * 
+     * @return Response
+     */
+    public function add(int $id, Request $request): Response
+    {
+        $product = $this->productRepo->find($id);
+
+        if(!$product) {
+            throw $this->createNotFoundException("Le produit $id n'existe pas");
+        }
+
+        $this->cartService->add($id);
+
+        $this->addFlash(
+            "success",
+            "Le produit a bien été ajouté au panier"
+        );
+        
+        if($request->query->get('returnToCart')) {
+            return $this->redirectToRoute("cart_show");
+        }
+
+       return $this->redirectToRoute("product_show",[
+           "slug" => $product->getSlug()
+       ]);
+    }
+
+    /**
+     * Permet d'afficher le panier
+     *
+     * @Route("/customer/cart", name="cart_show")
+     * 
+     * @return Response
+     */
+    public function show(): Response
+    {
+        $detailedCart = $this->cartService->getDetailedCart();
+        $total        = $this->cartService->getTotal();
+
+        return $this->render("cart/index.html.twig",[
+            "items" => $detailedCart,
+            "total" => $total
+            
+        ]);
+    }
+
+    /**
+     * Permet de supprimer un produit du panier
+     * 
+     * @Route("/customer/cart/delete/{id<\d+>}", name="cart_delete")
+     *
+     */
+    public function delete($id)
+    {
+        $product = $this->productRepo->find($id);
+
+        if(!$product) {
+            return $this->createNotFoundException("Le produit $id n'existe pas");
+        }
+
+        $this->cartService->remove($id);
+
+        $this->addFlash(
+            "danger",
+            "Le produit a bien été supprimer"
+        );
+
+        return $this->redirectToRoute("cart_show");
+    }
+
+    /**
+     * Permet de décrémenter le nombre de produit
+     *
+     * @Route("/customer/cart/decrement/{id<\d+>}", name="cart_decrement")
+     * 
+     * @return void
+     */
+    public function decrement($id)
+    {
+        $product = $this->productRepo->find($id);
+
+        if(!$product) {
+            return $this->createNotFoundException("Le produit $id n'existe pas");
+        }
+
+        $this->cartService->decrement($id);
+
+        $this->addFlash(
+            "success",
+            "Le produit a bien été décrémenté"
+        );
+
+        return $this->redirectToRoute("cart_show");
+    }
+
+    
+}
